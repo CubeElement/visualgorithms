6571ea925a2a5a66e61cefdb19f6502dc60ecaee
cubeelement <cubeelement@protonmail.com>
2021-09-11 09:17:56 +0200

    Working algorithm, hardcoded start/end without ui-interactions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,16 +4,20 @@
 #include <QLabel>
 #include <QSizePolicy>
 #include <QDebug>
-#include <iostream>
 #include <QObject>
 #include <QPushButton>
 
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    , ui(new Ui::MainWindow), GRID_SIZE(10)
 {
     ui->setupUi(this);
-    this->createGrid(10);
+    this->createGrid(GRID_SIZE);
 }
 
 MainWindow::~MainWindow()
@@ -34,18 +38,14 @@ void MainWindow::createGrid(int n)
         for ( int j = 0; j < n; j++ )
         {
             QPushButton* element = new QPushButton(grid);
-            element->setGeometry(i*30, j*30, 29, 29);
+            element->setGeometry(j*30, i*30, 29, 29);
             element->setStyleSheet("QPushButton {"
-                                   "border-style: solid;"
-                                   "border-width: 1px;"
-                                   "border-color: black;"
                                    "background-color: rgba(46, 204, 113, 0.4); border: none;"
                                    "}");
             element->setText(QString::number(counter));
             counter++;
             QObject::connect(element, &QPushButton::pressed,
                              this, &MainWindow::pressedButton);
-
         }
     }
 }
@@ -54,9 +54,62 @@ void MainWindow::pressedButton()
 {
     QPushButton *button = qobject_cast<QPushButton *> (sender());
     button->setStyleSheet("QPushButton {"
-                                   "border-style: solid;"
-                                   "border-width: 1px;"
-                                   "border-color: black;"
-                                   "background-color: rgba(80, 50, 113, 0.4); border: none;"
-                                   "}");
+                              "background-color: rgba(80, 50, 113, 0.4); border: none;"
+                              "}");
+    std::list <int> blocked;
+    findPath(21, 99, blocked);
+}
+
+std::vector <int> MainWindow::findPath(int start, int end, std::list <int> blocked)
+{
+    std::vector <int> previous(GRID_SIZE^2);
+    std::list <int> queue;
+    std::list <int> visited;
+    queue.push_back(start);
+    visited.push_back(start);
+
+    int current = start;
+    bool foundEnd = false;
+    while ( !queue.empty() && !foundEnd)
+    {
+        current = queue.front();
+        queue.pop_front();
+        for ( int neighbor : neighbors(current, GRID_SIZE) )
+        {
+            if ( std::count(visited.begin(), visited.end(), neighbor) == 0)
+            {
+                queue.push_back(neighbor);
+                visited.push_back(neighbor);
+                previous[neighbor] = current;
+            }
+            if ( neighbor == end ) { foundEnd = true; break; }
+        }
+    }
+    printShortestPath(previous);
+    return previous;
+}
+
+void MainWindow::printShortestPath(std::vector <int> prev)
+{
+    int cell = prev.back();
+    while ( cell != 0 )
+    {
+        qDebug() << cell;
+        cell = prev[cell];
+    }
+}
+
+std::list <int> MainWindow::neighbors(int cell, int const GRID_SIZE)
+{
+    std::list <int> adjacent_cells;
+
+    int coor_x = (cell+1 <= GRID_SIZE ?  cell : cell % GRID_SIZE);
+    int coor_y = cell / GRID_SIZE;
+
+    if ( coor_y - 1 >= 0 ) adjacent_cells.push_back(cell - GRID_SIZE);
+    if ( coor_x + 1 < GRID_SIZE ) adjacent_cells.push_back( cell + 1 );
+    if ( coor_y + 1 < GRID_SIZE ) adjacent_cells.push_back( cell + GRID_SIZE );
+    if ( coor_x - 1 >= 0 ) adjacent_cells.push_back( cell - 1 );
+
+    return adjacent_cells;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <iostream>
+#include <list>
+#include <vector>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -21,7 +24,14 @@ private:
 public slots:
     void pressedButton();
 
-private:
-    void createGrid(int);
+public:
+    int const GRID_SIZE;
+    void createGrid(int GRID_SIZE);
+    std::vector <int> findPath(int start, int end, std::list <int>);
+    std::list <int> neighbors(int cell, int GRID_SIZE);
+    void printShortestPath(std::vector <int> previous_list);
 };
+
+
+
 #endif // MAINWINDOW_H
